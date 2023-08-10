5ea468e79cf6676e71264e9d0d22c30d1a481777
Panayiotis Halouvas <phalouvas@kainotomo.com>
2023-08-10 02:48:13 +0000

    before adding point of sale

--- pos_screen/pos_screen/page/pos_screen/pos_screen.js
+++ pos_screen/pos_screen/page/pos_screen/pos_screen.js
@@ -25,85 +25,29 @@ class PosScreen {
 	}
 
 	make_filters() {	
-		this.get_pos_profiles();	
-		this.view = this.page.add_field({
-			label: __("View"),
-			fieldname: "view",
-			fieldtype: "Select",
-			options: ["Jobs", "Workers"],
-			default: "Jobs",
-			change: () => {
-				this.queue_timeout.toggle(this.view.get_value() === "Jobs");
-				this.job_status.toggle(this.view.get_value() === "Jobs");
-			},
-		});
-		this.queue_timeout = this.page.add_field({
-			label: __("Queue"),
-			fieldname: "queue_timeout",
-			fieldtype: "Select",
-			options: [
-				{ label: "All Queues", value: "all" },
-				{ label: "Default", value: "default" },
-				{ label: "Short", value: "short" },
-				{ label: "Long", value: "long" },
-			],
-			default: "all",
-		});
-		this.job_status = this.page.add_field({
-			label: __("Job Status"),
-			fieldname: "job_status",
-			fieldtype: "Select",
-			options: [
-				{ label: "All Jobs", value: "all" },
-				{ label: "Queued", value: "queued" },
-				{ label: "Deferred", value: "deferred" },
-				{ label: "Started", value: "started" },
-				{ label: "Finished", value: "finished" },
-				{ label: "Failed", value: "failed" },
-			],
-			default: "all",
-		});
-		this.auto_refresh = this.page.add_field({
-			label: __("Auto Refresh"),
-			fieldname: "auto_refresh",
-			fieldtype: "Check",
-			default: 1,
-			change: () => {
-				if (this.auto_refresh.get_value()) {
-					this.refresh_jobs();
-				}
-			},
-		});
-	}
-
-	show() {
-		this.refresh_jobs();
-	}
-
-	get_pos_profiles() {
 		frappe.call({
 			method: "pos_screen.pos_screen.page.pos_screen.pos_screen.get_pos_profiles",
 			callback: (r) => {
-				let { status } = r.message;
-				if (status === "active") {
-					this.page.set_indicator(__("Scheduler: Active"), "green");
-				} else {
-					this.page.set_indicator(__("Scheduler: Inactive"), "red");
-				}
+				this.pos_profiles = this.page.add_field({
+					label: __("POS Profile"),
+					fieldname: "name",
+					fieldtype: "Select",
+					options: r.message,
+					change: () => {
+						this.refresh_jobs();
+					},
+				});
 			},
-		});
+		});	
+	}
+
+	show() {
+		//this.refresh_jobs();
 	}
 
 	refresh_jobs() {
-		let view = this.view.get_value();
-		let args;
-		let { queue_timeout, job_status } = this.page.get_form_values();
-		if (view === "Jobs") {
-			args = { view, queue_timeout, job_status };
-		} else {
-			args = { view };
-		}
-
+		let pos_profile = this.pos_profiles.get_value();
+		let args = {pos_profile}
 		this.page.add_inner_message(__("Refreshing..."));
 		frappe.call({
 			method: "pos_screen.pos_screen.page.pos_screen.pos_screen.get_info",
@@ -111,15 +55,14 @@ class PosScreen {
 			callback: (res) => {
 				this.page.add_inner_message("");
 
-				let template = view === "Jobs" ? "pos_screen" : "background_workers";
+				let template = "pos_screen";
 				this.$content.html(
 					frappe.render_template(template, {
 						jobs: res.message || [],
 					})
 				);
 
-				let auto_refresh = this.auto_refresh.get_value();
-				if (frappe.get_route()[0] === "pos_screen" && auto_refresh) {
+				if (frappe.get_route()[0] === "pos_screen" && pos_profiles) {
 					setTimeout(() => this.refresh_jobs(), 2000);
 				}
 			},
--- pos_screen/pos_screen/page/pos_screen/pos_screen.py
+++ pos_screen/pos_screen/page/pos_screen/pos_screen.py
@@ -15,50 +15,13 @@ JOB_COLORS = {"queued": "orange", "failed": "red", "started": "blue", "finished"
 
 
 @frappe.whitelist()
-def get_info(view=None, queue_timeout=None, job_status=None) -> list[dict]:
-	jobs = []
-
-	def add_job(job: "Job", queue: str) -> None:
-
-		if job.kwargs.get("site") == frappe.local.site:
-			job_info = {
-				"job_name": job.kwargs.get("kwargs", {}).get("playbook_method")
-				or job.kwargs.get("kwargs", {}).get("job_type")
-				or str(job.kwargs.get("job_name")),
-				"status": job.get_status(),
-				"queue": queue,
-				"creation": convert_utc_to_user_timezone(job.created_at),
-				"color": JOB_COLORS[job.get_status()],
-			}
-
-			if job.exc_info:
-				job_info["exc_info"] = job.exc_info
-
-			jobs.append(job_info)
-
-	if view == "Jobs":
-		queues = get_queues()
-		for queue in queues:
-			for job in queue.jobs:
-				if job_status != "all" and job.get_status() != job_status:
-					return
-				if queue_timeout != "all" and not queue.name.endswith(f":{queue_timeout}"):
-					return
-				add_job(job, queue.name)
-
-	elif view == "Workers":
-		workers = get_workers()
-		for worker in workers:
-			current_job = worker.get_current_job()
-			if current_job:
-				if hasattr(current_job, "kwargs") and current_job.kwargs.get("site") == frappe.local.site:
-					add_job(current_job, current_job.origin)
-				else:
-					jobs.append({"queue": worker.name, "job_name": "busy", "status": "", "creation": ""})
-			else:
-				jobs.append({"queue": worker.name, "job_name": "idle", "status": "", "creation": ""})
-
-	return jobs
+def get_info(pos_profile=None) -> list[dict]:
+	entry_list = frappe.get_list("POS Opening Entry", filters={"docstatus": 1, "status": "Open", "pos_profile": pos_profile}, fields=["name", "status"])
+	if entry_list:
+		pos_opening_entry = frappe.get_doc("POS Opening Entry", entry_list[0].name)
+		pos_invoice_list = frappe.get_list("POS Invoice")
+		pos_invoice = frappe.get_doc("POS Invoice", pos_invoice_list[0].name)
+	return entry_list
 
 
 @frappe.whitelist()
@@ -73,6 +36,4 @@ def remove_failed_jobs():
 
 @frappe.whitelist()
 def get_pos_profiles():
-	if is_scheduler_inactive():
-		return {"status": "inactive"}
-	return {"status": "active"}
+	return frappe.get_list("POS Profile", filters={"Disabled": "No"}, fields=["name as label", "name as value"])
