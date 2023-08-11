c802f9b034d621788877e0b05b5d9ac590be7439
Panayiotis Halouvas <phalouvas@kainotomo.com>
2023-08-11 05:07:15 +0000

    get group items from pos profile

--- pos_screen/public/js/extend-point-of-sale.js
+++ pos_screen/public/js/extend-point-of-sale.js
@@ -12,26 +12,13 @@ frappe.require('point-of-sale.bundle.js', function () {
             const me = this;
             const doc = me.events.get_frm().doc;
             this.$component.find('.item-group-field').html('');
-    
+            
             this.item_group_field = frappe.ui.form.make_control({
                 df: {
                     label: __('Item Group'),
                     fieldtype: 'Select',
                     default: "All Item Groups",
-                    options: [
-                        {name: "All Item Groups", value: "All Item Groups"}, 
-                        {name: "Αλμυρά Σνακ", value: "Αλμυρά Σνακ"}, 
-                        {name: "KERASTIKA", value: "KERASTIKA"},
-                        {name: "Αλμυρά Σνακ", value: "Αλμυρά Σνακ"},
-                        {name: "PSOMIA", value: "PSOMIA"},
-                        {name: "ZUGIZOMENA", value: "ZUGIZOMENA"},
-                        {name: "FYLLARIKA", value: "FYLLARIKA"},
-                        {name: "SLICE FRANTZIOLAKIA", value: "SLICE FRANTZIOLAKIA"},
-                        {name: "SANDWICH", value: "SANDWICH"},
-                        {name: "Παξιμάδια", value: "Παξιμάδια"},
-                        {name: "Κέικς", value: "Κέικς"},
-                        {name: "Πίττες", value: "Πίττες"}
-                    ],
+                    options: [],
                     onchange: function() {
                         me.item_group = this.value;
                         !me.item_group && (me.item_group = me.parent_item_group);
@@ -44,8 +31,33 @@ frappe.require('point-of-sale.bundle.js', function () {
             });
             
             this.item_group_field.toggle_label(false);
-    
-            this.attach_clear_btn();
+
+            const selectField = this.item_group_field;
+            frappe.call({
+                method: 'frappe.desk.search.search_link',
+                args: {
+                    doctype: 'Item Group',
+                    txt: '',
+                    reference_doctype: '',
+                    query: 'erpnext.selling.page.point_of_sale.point_of_sale.item_group_query',
+                    filters: {
+                        pos_profile: doc ? doc.pos_profile : ''
+                    }
+                },
+                callback: function (response) {
+                    if (response) {
+                        selectField.df.options = '';
+
+                        for (let i = 0; i < response.results.length; i++) {
+                            selectField.df.options += response.results[i].value + (i !== response.results.length - 1 ? '\n' : '');
+                        }
+
+                        console.log(selectField.df.options);
+
+                        selectField.refresh();                    
+                    }
+                },
+            });
         }
 
     };
