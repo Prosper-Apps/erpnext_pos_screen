d9b02c9a1dae8ba4d136144868c3fc466bc4b53d
Panayiotis Halouvas <phalouvas@kainotomo.com>
2023-10-09 11:58:11 +0000

    Remove confirm step during pos checkout Fixes #6

--- pos_screen/public/js/extend-point-of-sale.js
+++ pos_screen/public/js/extend-point-of-sale.js
@@ -162,6 +162,19 @@ frappe.require('point-of-sale.bundle.js', function () {
                 return item_row;
             }
         }
+
+        init_payments() {
+            super.init_payments();
+            this.payment.events.submit_invoice = () => {	
+                this.frm.savesubmit()
+                    .then((r) => {
+                        this.toggle_components(false);
+                        this.order_summary.toggle_component(true);
+                        this.order_summary.load_summary_of(this.frm.doc, true);
+                        this.order_summary.print_receipt();
+                    });
+            }
+        }
     };
 
     erpnext.PointOfSale.ItemSelector = class KainotomoItemSelector extends erpnext.PointOfSale.ItemSelector {
